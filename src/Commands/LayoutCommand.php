28395ff9f0f2fa294186dfdaf0eee90ce6f0ef89
Stef van Esch <stef@marshmallow.dev>
2020-07-01 11:47:16 +0200

    Removed unused "use"

--- src/Commands/LayoutCommand.php
+++ src/Commands/LayoutCommand.php
@@ -4,7 +4,6 @@ namespace Marshmallow\Nova\Flexible\Commands;
 
 use Illuminate\Support\Str;
 use Illuminate\Console\Command;
-use Illuminate\Support\Facades\Artisan;
 
 class LayoutCommand extends Command
 {
