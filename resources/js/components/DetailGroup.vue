7fdc26d3a5815253d0fa0665923c82583298cbfa
LTKort <lars.kort94@gmail.com>
2022-04-26 14:40:46 +0200

    errors & style

--- resources/js/components/DetailGroup.vue
+++ resources/js/components/DetailGroup.vue
@@ -1,7 +1,8 @@
 <template>
-    <div :class="componentStyle">
+    <div :class="componentStyle" :dusk="'detail-' + attribute + '-' + index">
         <div :class="titleStyle" v-if="group.title">
-            <span class="block float-left border-r border-40 pr-4 mr-4"
+            <span
+                class="block float-left border-r border-gray-100 dark:border-gray-700  pr-4 mr-4"
                 ><!--
              --><span class="text-60 text-xs">#</span
                 ><!--
@@ -18,7 +19,7 @@
             :resource-test="resourceId"
             :resource="resource"
             :field="item"
-            :errors="validationErrors"
+            :validation-errors="validationErrors"
             :class="{
                 'remove-bottom-border': index == group.fields.length - 1,
             }"
@@ -29,6 +30,7 @@
 <script>
     export default {
         props: [
+            "attribute",
             "group",
             "index",
             "last",
@@ -42,7 +44,12 @@
                 return this.last ? [] : ["border-b border-50 pb-4 mb-4"];
             },
             titleStyle() {
-                return ["pb-4", "border-b", "border-40"];
+                return [
+                    "pb-4",
+                    "border-b",
+                    "border-gray-100",
+                    "dark:border-gray-700",
+                ];
             },
         },
     };
